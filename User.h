4154cc15fe883ef7d9da0ccab6278a3dbf8c1763
iBlacKings <juangilagudelo@correounivalle.edu.co>
2024-11-03 22:38:24 -0500

    Start Development of User Class

---
+++ User.h
@@ -0,0 +1,21 @@
+//
+// Created by gilju on 3/11/2024.
+//
+
+#ifndef USER_H
+#define USER_H
+#include <iostream>
+
+
+class User {
+private:
+    std::string name;
+    int userID;
+
+public:
+
+};
+
+
+
+#endif //USER_H
